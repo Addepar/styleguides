a967ade5e67c218aba289d27251e95626964d3de
Christopher Garrett <opensource@pzuraq.com>
2018-04-17 10:12:18 -0700

    [feat] Implements Prettier and adds better READMEs
    Implements Prettier, and removes all of the extra eslint rules made
    redundant by Prettier. Also removes the `babel-eslint` and `eslint`
    dependencies, those should be provided by the consuming app/addon or
    another package.
    
    Changes sass-lint to match as well, and adds READMEs for Prettier and
    Sass Lint packages. This has been tested in our apps, should be good
    to go.

--- packages/@addepar/eslint-config/ember.js
+++ packages/@addepar/eslint-config/ember.js
@@ -5,14 +5,42 @@
 */
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   extends: [
     'plugin:ember/recommended',
-    'plugin:ember-best-practices/recommended'
+    'plugin:ember-best-practices/recommended',
   ],
-  rules: { }
+  rules: { },
+  overrides: [
+    // node files
+    {
+      files: [
+        'index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js',
+      ],
+      excludedFiles: [
+        'app/**',
+        'addon/**',
+        'tests/dummy/app/**',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015,
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      }),
+    },
+  ],
 };
---
+++ packages/@addepar/prettier-config/README.md
@@ -0,0 +1,22 @@
+# @addepar/prettier-config
+
+This is the Prettier config used for Javascript projects at Addepar.
+
+## Using the plugin
+
+### Installation
+
+Install the plugin as a dev dependency in your project.
+
+```bash
+yarn add --dev @addepar/prettier-config
+```
+
+### Configuration
+
+Re-export the config in `.prettierrc.js`
+
+```js
+// .prettierrc.js
+module.exports = require('@addepar/prettier-config');
+```
