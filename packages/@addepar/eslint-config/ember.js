e49a2fb3c33a9100cd80dbed528ea6a40ab9a6e7
Cory Forsyth <cory.forsyth@gmail.com>
2019-07-12 13:12:31 -0400

    [CHORE] Ensure addon-test-support/ is eslint'd
    Ensure that the ember eslint config doesn't skip files in `addon-test-support/`

--- packages/@addepar/eslint-config/ember.js
+++ packages/@addepar/eslint-config/ember.js
@@ -27,6 +27,7 @@ module.exports = {
       excludedFiles: [
         'app/**',
         'addon/**',
+        'addon-test-support/**',
         'tests/dummy/app/**',
       ],
       parserOptions: {
